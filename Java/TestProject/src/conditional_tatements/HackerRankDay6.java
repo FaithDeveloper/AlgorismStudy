b0b764ee229e395059ba45838086cd3a5198ad1d
신규찬 <dev.faithpeople@gmail.com>
2020-05-20 23:59:46 +0900

    Day 5, 6

---
+++ Java/TestProject/src/conditional_tatements/HackerRankDay6.java
@@ -0,0 +1,37 @@
+package conditional_tatements;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class HackerRankDay6 {
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int n = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		String[] input = new String[n]; 
+		for(int i = 0; i<n; i++) {
+			input[i] = scanner.nextLine();
+		}
+		
+		for(int i = 0; i<n; i++) {
+			 char[] charArray = input[i].toCharArray();
+			 String oddString = "";
+			 String evenString = "";
+			 for(int j = 0; j < charArray.length; j++) {
+				 if(j %2 == 0) {
+					 oddString += charArray[j];
+				 }else {
+					 evenString += charArray[j];
+				 }
+			 }
+			 System.out.println(String.format("%s %s", oddString, evenString));
+		}
+		scanner.close();
+	}
+
+}
---
+++ Java/TestProject/src/conditional_tatements/HackerRankDay7.java
@@ -0,0 +1,51 @@
+package conditional_tatements;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class HackerRankDay7 {
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+	    int n = scanner.nextInt();
+	    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+	
+	    int[] arr = new int[n];
+	
+	    String[] arrItems = scanner.nextLine().split(" ");
+	    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+	
+	    for (int i = 0; i < n; i++) {
+	        int arrItem = Integer.parseInt(arrItems[i]);
+	        arr[i] = arrItem;
+	    }
+		
+	    scanner.close();
+	    
+	    // 가운데 index 기준으로 좌우 스위칭한다.
+	    int temp = 0;
+    	int middle = arr.length / 2;
+    	for (int i = 0; i < middle; i++) {
+	        temp = arr[i];
+	        arr[i] = arr[ arr.length - 1 - i];
+	        arr[ arr.length - 1 - i] = temp;
+	    }
+    	String output = "";
+    	 for (int i = 0; i < n; i++) {
+ 	       if(output.isEmpty()) {
+ 	    	   output = String.valueOf(arr[i]);
+ 	       }else {
+ 	    	  output = output + " " + String.valueOf(arr[i]);
+ 	       }
+ 	    }
+    	 System.out.println(output);	    
+
+	}
+
+}
