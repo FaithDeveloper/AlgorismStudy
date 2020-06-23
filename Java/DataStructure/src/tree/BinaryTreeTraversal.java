79141ea9388b7bc87dd1fd930f57b6b9ca401e46
FaithDeveloper <dev.faithpeople@gmail.com>
2020-06-23 16:39:10 +0900

    Tree 업데이트

---
+++ Java/DataStructure/src/tree/BinaryTreeTraversal.java
@@ -0,0 +1,19 @@
+package tree;
+
+/**
+ * 	    (1)
+ *      ↙  ↘︎
+ *    (2)  (3)
+ *   ↙  ↘︎
+ * (4)  (5)
+ * @author kyuchan.shin
+ *
+ */
+public class BinaryTreeTraversal {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
